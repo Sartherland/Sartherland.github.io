5a14f8e01ff48c170f1385190be86e5d8d6cfb92
Ec0y <1176139845@qq.com>
2022-07-03 21:37:42 +0800

    Site updated: 2022-07-03 21:37:42

--- js/music.js
+++ js/music.js
@@ -4,7 +4,14 @@ const ap = new APlayer({
     audio: [{
         name: 'comethru',
         artist: 'Jeremy Zucker / Bea Miller',
-        url: 'https://m801.music.126.net/20220703194136/bce17347937a5aa5ab067834244c220d/jdyyaac/obj/w5rDlsOJwrLDjj7CmsOj/13256834629/6bc0/9823/70a6/fc7b828f92b3ef1eb6e2cb857e2eb00a.m4a',
-        cover: 'https://p1.music.126.net/mmMLF6_a1yDnzGO1LVyCeg==/109951163926564641.jpg?param=34y34',
-    }]
+        url: 'https://m801.music.126.net/20220703213817/bd6fd726000ef3057bdccbec748edfd9/jdyyaac/obj/w5rDlsOJwrLDjj7CmsOj/13256834629/6bc0/9823/70a6/fc7b828f92b3ef1eb6e2cb857e2eb00a.m4a',
+        cover: 'http://p1.music.126.net/mmMLF6_a1yDnzGO1LVyCeg==/109951163926564641.jpg?param=130y130',
+    },
+   {
+            name: "Baby I'm Yours",
+            artist: 'Breakbot',
+            url: 'https://m10.music.126.net/20220703215233/e34145293d21fde0fad5305431e27582/yyaac/obj/wonDkMOGw6XDiTHCmMOi/3537659166/8f9a/6dd2/9af0/890714497a00af82310169e39cb541c3.m4a',
+            cover: 'http://p1.music.126.net/9YvEXwlECxr3o-lZp-nmLw==/109951165212867712.jpg?param=130y130',
+     }
+    ]
 });
