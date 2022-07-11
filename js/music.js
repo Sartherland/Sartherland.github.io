972d8f82a4767a6f1d4a0c5cfd5ad8c5a0936c4c
Ec0y <1176139845@qq.com>
2022-07-11 10:58:03 +0800

    Site updated: 2022-07-11 10:58:03

--- js/music.js
+++ js/music.js
@@ -4,6 +4,12 @@ const ap = new APlayer({
     autoplay: true,
     audio: [
     {
+            name: "shelveme",
+            artist: 'Tai Verdes',
+            url: '/music/sheluvme.m4a',
+            cover: 'http://p2.music.126.net/SvM1dpYNtWxT1K9Otg7JGA==/109951167549562317.jpg?param=130y130',
+     },
+    {
             name: "LAst dAy oN EaRTh",
             artist: 'Tai Verdes',
             url: 'https://dl.stream.qqmusic.qq.com/C400003IIXxt41Zyd3.m4a?guid=1578982482&vkey=B51631749169BF47C164B6A53325EE576BB1CF7972686305237A3167AA40EC3EF37EE569A1801D5918F10293221A886937DBED04CDB4B2D1&uin=1176139845&fromtag=120032',
